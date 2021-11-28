123e501e0063774e20c5dc8f5b3fb9d426d9c9d3
unknown <slavko.vuletic92@gmail.com>
2021-11-28 22:45:42 +0100

    yea

--- trigger_notification.js
+++ trigger_notification.js
@@ -6,5 +6,7 @@ v_execute("ls");
 // Check git status
 (async()=>{
 
-    console.log(await v_execute("bash -c \"cd Toaster-2.0__win10 ; bash NEW_TOAST_restart.sh ;\""));
+    //console.log(await v_execute("bash -c \"cd Toaster-2.0__win10 ; bash NEW_TOAST_restart.sh ;\""));
+
+    v_execute("bash -c \"cd Toaster-2.0__win10 ; powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile -WindowStyle Hidden -File New-ToastNotification.ps1 -Config good_morning_demo_01.xml \"")
 })();
